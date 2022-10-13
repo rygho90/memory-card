58a7ee239e0ec5ba884e073832ea6a52ed3bad16
Ryan <rygho90@gmail.com>
2022-10-13 07:48:33 -0400

    Populate full movie list

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,16 @@ import '../css/app.css'
 import MovieList from "./MovieList";
 import lotr from "../images/lotr.jpg";
 import lebowski from "../images/lebowski.jpg"
+import princessBride from "../images/princess-bride.jpg"
+import interstellar from "../images/interstellar.jpg"
+import goodfellas from "../images/goodfellas.jpg"
+import nakedGun from "../images/naked-gun.jpg"
+import departed from "../images/departed.jpg"
+import dune from "../images/dune.jpg"
+import montyPython from "../images/monty-python.jpg"
+import prestige from "../images/prestige.jpg"
+import airplane from "../images/airplane.jpg"
+import shrek from "../images/shrek.jpg"
 
 export default function App() {
   const [currScore, setCurrScore] = useState();
@@ -39,5 +49,55 @@ const movies = [
     id: 2,
     name: "The Big Lebowski",
     image: lebowski,
-  }
+  },
+  {
+    id: 3,
+    name: "The Princess Bride",
+    image: princessBride,
+  },
+  {
+    id: 4,
+    name: "Interstellar",
+    image: interstellar,
+  },
+  {
+    id: 5,
+    name: "Goodfellas",
+    image: goodfellas,
+  },
+  {
+    id: 6,
+    name: "Naked Gun",
+    image: nakedGun,
+  },
+  {
+    id: 7,
+    name: "The Departed",
+    image: departed,
+  },
+  {
+    id: 8,
+    name: "Dune",
+    image: dune,
+  },
+  {
+    id: 9,
+    name: "Monty Python & The Holy Grail",
+    image: montyPython,
+  },
+  {
+    id: 10,
+    name: "The Prestige",
+    image: prestige,
+  },
+  {
+    id: 11,
+    name: "Airplane!",
+    image: airplane,
+  },
+  {
+    id: 12,
+    name: "Shrek",
+    image: shrek,
+  },
 ];
